31b72a7128e0444432d657fc909a0d411efaec7b
zhangjr <zhangjr@singula-point.com>
2017-10-23 20:31:55 +0800

    123

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <fstream>? 
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
-	
+	string ls_time = system("time"); 
+	string path = m_strPath + '\\' + ls_time+'.txt';
+	fstream file;
+	file.open( path, ios::out );
+	if (!file)
+	{
+		return false;
+	}
+	file.close();
+	ofstream myfile("ls_time.txt",ios::out);  
+ 	if(!myfile)
+ 		{
+			cout<<"error !";
+		 }
+ 	else
+		{
+			myfile<<"Auto test result!"<<endl;
+			myfile.close();
+		}
+
 	return 0;
 
 }
